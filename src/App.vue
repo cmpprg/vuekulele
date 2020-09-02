535fcacd6a98e21e53450dbd4ce3fda003543ce2
Ryan Camp <campryan@comcast.net>
2020-09-02 16:24:15 -0600

    Create ukulele component and implement in App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Ukulele/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Ukulele from './components/pages/Ukulele.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Ukulele
   }
 }
 </script>
---
+++ src/components/pages/Ukulele.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>This is the Ukulele Page</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Ukulele"
+  }
+</script>
+
+<style scoped>
+  
+</style>
