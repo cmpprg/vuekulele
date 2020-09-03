216ad97c141be0a369358106b48fb5c624747c8d
Ryan Camp <campryan@comcast.net>
2020-09-03 11:47:26 -0600

    Create LandingPage component

--- src/components/Neck.vue
+++ src/components/Neck.vue
@@ -1,5 +1,20 @@
 <template>
   <table class="neck">
+    <tr>
+      <th>OPEN</th>
+      <th>1<sup>st</sup> FRET</th>
+      <th>2<sup>st</sup> FRET</th>
+      <th>3<sup>st</sup> FRET</th>
+      <th>4<sup>st</sup> FRET</th>
+      <th>5<sup>st</sup> FRET</th>
+      <th>6<sup>st</sup> FRET</th>
+      <th>7<sup>st</sup> FRET</th>
+      <th>8<sup>st</sup> FRET</th>
+      <th>9<sup>st</sup> FRET</th>
+      <th>10<sup>st</sup> FRET</th>
+      <th>11<sup>st</sup> FRET</th>
+    </tr>
+
     <String openNote='A4'/>
     <String openNote='E4'/>
     <String openNote='C4'/>
@@ -24,5 +39,9 @@
     border-radius: 10px;
     width: 100%;
     background-color: #1e140a;
+  }
+
+  th {
+    color: #D3D3D3;
   }
 </style>
---
+++ src/components/pages/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>I am the Landing Page!</p>
+</template>
+
+<script>
+  export default {
+    name: "LandingPage"
+  }
+</script>
+
+<style scoped>
+
+</style>
